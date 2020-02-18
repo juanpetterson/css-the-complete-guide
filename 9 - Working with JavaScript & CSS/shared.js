d5213f97c84105080d51784fe2ea6ef7630a5ac0
juanpetterson <juan.petterson@gmail.com>
2020-02-18 07:17:30 -0300

    Implemented javascript code to show/hide the choose plan modal

---
+++ 9 - Working with JavaScript & CSS/shared.js
@@ -0,0 +1,19 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal button');
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function() {
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+  });
+}
+
+modalNoButton.addEventListener('click', closeModal);
+backdrop.addEventListener('click', closeModal);
+
+function closeModal() {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+}
