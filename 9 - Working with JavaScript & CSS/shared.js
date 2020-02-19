cd9ff05245ef797244c5bfccc4df55a95234759f
juanpetterson <juan.petterson@gmail.com>
2020-02-18 21:32:40 -0300

    Implemented mobile button to show de sidebar

--- 9 - Working with JavaScript & CSS/shared.js
+++ 9 - Working with JavaScript & CSS/shared.js
@@ -2,18 +2,36 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var modalNoButton = document.querySelector('.modal button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function() {
-    backdrop.style.display = 'block';
-    modal.style.display = 'block';
+    backdrop.classList.add('open');
+    modal.classList.add('open');
   });
 }
 
-modalNoButton.addEventListener('click', closeModal);
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', closeModal);
+}
+
 backdrop.addEventListener('click', closeModal);
+toggleButton.addEventListener('click', showMobileNav);
 
 function closeModal() {
-  backdrop.style.display = 'none';
-  modal.style.display = 'none';
+  // backdrop.style.display = 'none';
+  // modal.style.display = 'none';
+  // mobileNav.style.display = 'none';
+  backdrop.classList.remove('open');
+  mobileNav.classList.remove('open');
+
+  if (modal) {
+    modal.classList.remove('open');
+  }
+}
+
+function showMobileNav() {
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 }
